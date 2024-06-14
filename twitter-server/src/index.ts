4e9a94b7ef3c347efd638893f14d2483de80d905
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-06-15 01:20:49 +0530

    server setup with graphQL

--- twitter-server/src/index.ts
+++ twitter-server/src/index.ts
@@ -1,1 +1,10 @@
-console.log("bhaskar");
+import { initialServer } from "./app";
+
+async function  init(){
+    const app = await initialServer()
+    app.listen(8000, ()=> console.log("Server started at port 8000")
+    )
+    
+}
+
+init()
