f45a2b9df8be1809cdc952db70b90a95ef95cf47
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-06-15 03:23:57 +0530

    reolvers and queries integrated in the index file in typeDefs

--- twitter-server/src/app/index.ts
+++ twitter-server/src/app/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { ApolloServer } from "@apollo/server";
 import {expressMiddleware} from "@apollo/server/express4"
 import bodyParser from "body-parser";
+import {User} from "./user"
 
 export async function initialServer(){
     const app = express()
@@ -10,14 +11,16 @@ export async function initialServer(){
 
     const graphqlServer = new ApolloServer({
         typeDefs: `
+        ${User.types}
         type Query{
-         sayHello: String
+           ${User.queries}
         
         }`,
 
         resolvers:{
             Query:{
-                sayHello: ()=> `Hello from graphql server`
+                // sayHello: ()=> `Hello from graphql server`
+                ...User.resolvers.queries
 
 
 
