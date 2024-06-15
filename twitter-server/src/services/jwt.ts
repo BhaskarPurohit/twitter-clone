432178345ec19e03c29f320205d2f6c9f93cf163
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-06-16 00:11:49 +0530

    jsonweb token

--- twitter-server/src/services/jwt.ts
+++ twitter-server/src/services/jwt.ts
@@ -1,5 +1,8 @@
 //jwt service
+import JWT from 'jsonwebtoken'
 import {prismaClient} from "../clients/db"
+
+const JWT_SECRET = "bhaskar$100k"
 class JWTService{
     public static async generateTokenForUser(userId: string){
         const user = await prismaClient.user.findUnique(
@@ -8,7 +11,15 @@ class JWTService{
             }
         )
         const payload = {
-            
+            id: user?.id,
+            email: user?.email
+
         }
+
+        const token = JWT.sign(payload, JWT_SECRET)
+
+        return token
     }
 }
+
+export default JWTService
