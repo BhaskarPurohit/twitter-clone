163207a3eefd9e5296c5ea12ac46e95d1165e02a
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-06-24 23:37:26 +0530

    auth token

--- twitter-server/src/app/user/resolvers.ts
+++ twitter-server/src/app/user/resolvers.ts
@@ -1,6 +1,9 @@
+import axios from "axios"
+
 const queries = {
     verifyGoogleToken: async(parent:any, {token}:{token:String}) =>{
         const googleToken = token
+        const googleOAuthURL = new URL('https//oauth2.googleapis.com/tokeninfo')
     }
 }
 
